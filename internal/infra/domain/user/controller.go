bdc8ea205bb27f1da86747abe9300373ad703a99
Diogo Cezar <8019111+diogocezar@users.noreply.github.com>
2024-12-21 11:33:46 -0300

    controller and repository

--- internal/infra/domain/user/controller.go
+++ internal/infra/domain/user/controller.go
@@ -1,6 +1,16 @@
 package user
 
-import "github.com/gofiber/fiber"
+import (
+	"errors"
+	"regexp"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type UserDTO struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
 
 type Controller struct {
 	repository *Repository
@@ -13,21 +23,65 @@ func MakeController(repository *Repository) *Controller {
 }
 
 func (c *Controller) Create(ctx *fiber.Ctx) error {
-	return nil
+	dto := new(UserDTO)
+
+	if err := ctx.BodyParser(dto); err != nil {
+		return ctx.
+			Status(fiber.StatusBadRequest).
+			JSON(fiber.Map{"error": "bad request"})
+	}
+
+	// TODO: move this validation to a separate structure
+	if err := func() error {
+		if dto.Name == "" {
+			return errors.New("missing name")
+		}
+
+		if len(dto.Name) > 100 {
+			return errors.New("invalid name")
+		}
+
+		if dto.Email == "" {
+			return errors.New("missing email")
+		}
+
+		emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+		re := regexp.MustCompile(emailRegex)
+		if !re.MatchString(dto.Email) {
+			return errors.New("invalid email")
+		}
+		return nil
+	}(); err != nil {
+		return ctx.
+			Status(fiber.StatusUnprocessableEntity).
+			JSON(fiber.Map{"error": err.Error()})
+	}
+
+	user, err := c.repository.Create(dto.Name, dto.Email)
+
+	if err != nil {
+		return ctx.
+			Status(fiber.StatusUnprocessableEntity).
+			JSON(fiber.Map{"error": err.Error()})
+	}
+
+	return ctx.
+		Status(fiber.StatusCreated).
+		JSON(user)
 }
 
 func (c *Controller) FindAll(ctx *fiber.Ctx) error {
-	return nil
+	panic("unimplemented")
 }
 
 func (c *Controller) FindOne(ctx *fiber.Ctx) error {
-	return nil
+	panic("unimplemented")
 }
 
 func (c *Controller) Update(ctx *fiber.Ctx) error {
-	return nil
+	panic("unimplemented")
 }
 
 func (c *Controller) Delete(ctx *fiber.Ctx) error {
-	return nil
+	panic("unimplemented")
 }
--- internal/infra/domain/user/repository.go
+++ internal/infra/domain/user/repository.go
@@ -1,11 +1,77 @@
 package user
 
-import "go-template/internal/infra/database"
+import (
+	"go-template/internal/infra/database"
+
+	"github.com/google/uuid"
+)
 
 type Repository struct {
 	database *database.Database
 }
 
-func (r *Repository) Create(user *User) error {
-	return r.db.Create(user).Error
+func (r *Repository) Create(name string, email string) (*User, error) {
+	user := User{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Email: email,
+	}
+
+	if _, err := r.database.Client.Exec("INSERT INTO user (id, name, email) VALUES (?, ?, ?)", user.ID, user.Name, user.Email); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *Repository) FindAll(name string, email string) ([]User, error) {
+	var users []User
+
+	rows, err := r.database.Client.Query("SELECT id, name, email FROM user")
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var user User
+
+		if err := rows.Scan(&user.ID, &user.Name, &user.Email); err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func (r *Repository) FindOne(id string) (*User, error) {
+	var user User
+
+	if err := r.database.Client.QueryRow("SELECT id, name, email FROM user WHERE id = ?", id).Scan(&user.ID, &user.Name, &user.Email); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *Repository) Update(id string, name string, email string) (*User, error) {
+	user := User{
+		ID:    id,
+		Name:  name,
+		Email: email,
+	}
+
+	if _, err := r.database.Client.Exec("UPDATE user SET name = ?, email = ? WHERE id = ?", user.Name, user.Email, user.ID); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *Repository) Delete(id string) error {
+	if _, err := r.database.Client.Exec("DELETE FROM user WHERE id = ?", id); err != nil {
+		return err
+	}
+	return nil
 }
